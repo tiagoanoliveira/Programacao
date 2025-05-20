fe29b65934c3d6290ea800a1de4f1e30014d2904
tiagoleic02 <up202007448@up.pt>
2025-05-20 11:22:31 +0100

    Comment invert.hpp and grayscale.hpp

--- include/Command/Grayscale.hpp
+++ include/Command/Grayscale.hpp
@@ -5,6 +5,13 @@
 
 namespace prog {
     namespace command {
+        /**
+         * @brief The Grayscale class implements a command to convert an image to grayscale
+         *
+         * This class transforms a color image into grayscale by replacing each
+         * pixel's color with an appropriate gray tone, usually based on the
+         * weighted average of the RGB components.
+         */
         class Grayscale : public Command {
         public:
             Grayscale();
--- include/Command/Invert.hpp
+++ include/Command/Invert.hpp
@@ -2,10 +2,17 @@
 #define PROJECT_INVERT_HPP
 
 #include "Command.hpp"
-#include "Color.hpp"
 
 namespace prog {
     namespace command {
+        /**
+         * @class Invert
+         * @brief Class that implements a command to invert the colors of an image
+         *
+        * This class inverts all color values of each pixel in the image,
+         * creating a negative effect where each RGB component is replaced
+         * with its complement (255 - original value).
+         */
         class Invert : public Command {
         public:
             Invert();
