6424f2f9e61eccb2ebabc88fb49ffb3a3f1e3896
tiagoleic02 <up202007448@up.pt>
2025-06-15 10:39:17 +0100

    Update struct to merge repository

---
+++ Projeto/build/CMakeFiles/runscrim.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/main/RunScrim.cpp" "CMakeFiles/runscrim.dir/main/RunScrim.cpp.o" "gcc" "CMakeFiles/runscrim.dir/main/RunScrim.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Color.cpp" "CMakeFiles/runscrim.dir/src/Color.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Color.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command.cpp" "CMakeFiles/runscrim.dir/src/Command.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Add.cpp" "CMakeFiles/runscrim.dir/src/Command/Add.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Add.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Blank.cpp" "CMakeFiles/runscrim.dir/src/Command/Blank.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Blank.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Chain.cpp" "CMakeFiles/runscrim.dir/src/Command/Chain.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Chain.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Crop.cpp" "CMakeFiles/runscrim.dir/src/Command/Crop.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Crop.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Fill.cpp" "CMakeFiles/runscrim.dir/src/Command/Fill.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Fill.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Grayscale.cpp" "CMakeFiles/runscrim.dir/src/Command/Grayscale.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Grayscale.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/HMirror.cpp" "CMakeFiles/runscrim.dir/src/Command/HMirror.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/HMirror.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Invert.cpp" "CMakeFiles/runscrim.dir/src/Command/Invert.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Invert.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Move.cpp" "CMakeFiles/runscrim.dir/src/Command/Move.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Move.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Open.cpp" "CMakeFiles/runscrim.dir/src/Command/Open.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Open.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Replace.cpp" "CMakeFiles/runscrim.dir/src/Command/Replace.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Replace.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Resize.cpp" "CMakeFiles/runscrim.dir/src/Command/Resize.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Resize.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Rotate.cpp" "CMakeFiles/runscrim.dir/src/Command/Rotate.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Rotate.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Save.cpp" "CMakeFiles/runscrim.dir/src/Command/Save.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Save.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Scale_up.cpp" "CMakeFiles/runscrim.dir/src/Command/Scale_up.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Scale_up.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/Slide.cpp" "CMakeFiles/runscrim.dir/src/Command/Slide.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Slide.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Command/VMirror.cpp" "CMakeFiles/runscrim.dir/src/Command/VMirror.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/VMirror.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Image.cpp" "CMakeFiles/runscrim.dir/src/Image.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Image.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Logger.cpp" "CMakeFiles/runscrim.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Logger.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/PNG.cpp" "CMakeFiles/runscrim.dir/src/PNG.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/PNG.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Scrim.cpp" "CMakeFiles/runscrim.dir/src/Scrim.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Scrim.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/ScrimParser.cpp" "CMakeFiles/runscrim.dir/src/ScrimParser.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/ScrimParser.cpp.o.d"
+  "/media/tiago-oliveira/Files/FEUP/Projeto/project_programacao/Projeto/src/Utils.cpp" "CMakeFiles/runscrim.dir/src/Utils.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
